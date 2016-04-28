2734dc9c2b5d69742e3ff7bd575112283c09f382
kelvinst <kelvin.stinghen@gmail.com>
2016-04-27 21:24:22 -0300

    Supporting the foundation

--- lib/generators/silmarails/install/install_generator.rb
+++ lib/generators/silmarails/install/install_generator.rb
@@ -1,6 +1,8 @@
 require 'rails/generators'
 module Silmarails
   class InstallGenerator < Rails::Generators::Base
+    class_option :framework, desc: 'Frontend framework to use (bootstrap, foundation).'
+
     def self.source_root
       @source_root ||= Silmarails::Engine.root.join "lib/generators/silmarails/install/files"
     end
@@ -9,6 +11,12 @@ module Silmarails
       templates = self.class.source_root.join "templates"
       Dir[templates.join "**/*.*"].each do |file|
         copy_file file, "lib/templates/#{file.gsub(templates.to_s, "")}"
+      end
+
+      framework = options[:framework] || "bootstrap"
+      framework_templates = self.class.source_root.join "#{framework}/templates"
+      Dir[framework_templates.join "**/*.*"].each do |file|
+        copy_file file, "lib/templates/#{file.gsub(framework_templates.to_s, "")}"
       end
 
       spec_support = self.class.source_root.join "spec/support"
